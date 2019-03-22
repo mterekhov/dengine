42ba7dbd7cc545f0573e73ce0a6f6958b0a4d4e7
Michael <mihail.terekhov@ctconsult.ru>
2019-03-22 18:02:56 +0300

    Initial Commit

---
+++ game/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  game
+//
+//  Created by Michael on 22.03.2019.
+//  Copyright © 2019 Michael. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
