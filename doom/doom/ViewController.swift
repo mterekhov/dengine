8e5c6065ff50c9aa0bd33f10eaa72ec62f091046
Michael Terekhov <m.terekhov@icloud.com>
2024-01-07 01:52:58 +0300

    SUBMODULES: adding empty macos app

---
+++ doom/doom/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  doom
+//
+//  Created by cipher on 07.01.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
